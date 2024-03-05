e2f20bfd189dca35678e6edfad48a480b15720d4
minjun Kim <k980128@pusan.ac.kr>
2024-03-05 21:23:55 +0900

    Made player to move only horizontal or vertical. added interactable ability to player

--- Player.cs
+++ Player.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 
 public class Player : MovingObject
 {
+    [SerializeField]
+    float Speed = 5f;
+
+
     float h;
     float v;
+    bool isHorizonMove;
+    Vector2 dirVec;//direction of where player is looking at
 
     Rigidbody2D rigid;
+    GameObject scanedObject;
 
     protected override void Start()
     {
@@ -26,10 +33,52 @@ public class Player : MovingObject
         h = Input.GetAxisRaw("Horizontal");
         v = Input.GetAxisRaw("Vertical");
 
+        bool hDown = Input.GetButtonDown("Horizontal");
+        bool vDown = Input.GetButtonDown("Vertical");
+        bool hUp = Input.GetButtonUp("Horizontal");
+        bool vUp = Input.GetButtonUp("Vertical");
+
+        if(hDown)
+            isHorizonMove = true;
+        else if(vDown)
+            isHorizonMove = false;
+        else if(hUp || vUp)
+            isHorizonMove = h != 0; //if h is not 0, isHorizonMove is true. (if h is 0, isHorizonMove is false.
+
+        //direction
+        if(vDown && v == 1)
+            dirVec = Vector3.up;
+        else if(vDown && v == -1)
+            dirVec = Vector3.down;
+        else if(hDown && h == 1)
+            dirVec = Vector3.right;
+        else if(hDown && h == -1)
+            dirVec = Vector3.left;
+
+
+        //scan object
+        if(Input.GetButtonDown("Jump") && scanedObject != null)
+        {
+            Debug.Log("스캔한 오브젝트: " + scanedObject.name);
+        }
     }
 
     void FixedUpdate()
     {
-        rigid.velocity = new Vector2(h, v);
+        //move
+        Vector2 moveVec = isHorizonMove ? new Vector2(h, 0) : new Vector2(0, v);
+        rigid.velocity = moveVec * Speed;
+
+        //ray
+        //Debug.DrawRay(this.transform.position, dirVec*0.7f, new Color(0, 1, 0)); <<레이를 실제로 보여줌
+        RaycastHit2D rayHit = Physics2D.Raycast(this.transform.position, dirVec, 0.7f, LayerMask.GetMask("Interactable"));
+        //chekcing if the ray hit the object that is in the layermask "Interactable" <<interactable 레이어는 상호 작용이 가능한 모든 오브젝트
+        if (rayHit.collider != null)
+        {
+            scanedObject = rayHit.collider.gameObject;
+        }
+        else
+            scanedObject = null;
+
     }
 }
