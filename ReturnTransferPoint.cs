66accfb7b8bc4d2ba23fd038df65460945c1fdbb
minjun Kim <k980128@pusan.ac.kr>
2024-05-21 20:27:12 +0900

    feat : when returning to same place in special condition, change the teleportation point.

---
+++ ReturnTransferPoint.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnTransferPoint : MonoBehaviour
+{
+    public string linkedMapName; //연결된 맵 이름
+
+
+    private void Start()
+    {
+        if (linkedMapName == Player.Instance.currentMapName) //이동전 받은 맵 이름이 transferMapName과 같다면
+        {
+            if(GameManager.Instance.storyScriptable.isStage2Completed && this.name == "Return")
+                Player.Instance.transform.position = this.transform.position; //해당 포탈의 위치에 플레이어 이동시켜주기
+
+        }
+    }
+}
--- TransferPoint.cs
+++ TransferPoint.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
 
 public class TransferPoint : MonoBehaviour
 {
@@ -9,6 +11,9 @@ public class TransferPoint : MonoBehaviour
 
     private void Start()
     {
+        if(SceneManager.GetActiveScene().name == "Stage1-1")
+            return;
+
         if (linkedMapName == Player.Instance.currentMapName) //이동전 받은 맵 이름이 transferMapName과 같다면
         {
             Player.Instance.transform.position = this.transform.position; //해당 포탈의 위치에 플레이어 이동시켜주기
