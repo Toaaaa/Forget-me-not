3356f628e0da5b76eb8eda631cc3449cce07be89
minjun Kim <k980128@pusan.ac.kr>
2024-08-05 17:58:23 +0900

    feat : Monster's status UI including buff,debuffs and elemental stack system finished.

--- Monster/MonsterStatUI.cs
+++ Monster/MonsterStatUI.cs
@@ -7,11 +7,10 @@ using UnityEngine;
 public class MonsterStatUI : MonoBehaviour//몬스터의 상단부에 표시될 버프 디버프 등의 상태 표시 ui
 {
     public TestMob thisMob;
-    //총 2+2+1으로 5칸의 버프 디버프 표시 ui.
-    public GameObject[] BuffsUI;//버프 표시 UI (attack,defense)//2개밖에 없다고 상정하고 코드 작성//수정시 유의.
-    public GameObject[] DebuffsUI;//디버프 표시 UI (defense,speed)
+    //총 3+1으로 4칸의 버프 디버프 표시 ui.
+    public GameObject[] BuffsUI;//버프 표시 UI (attack,defense,speed)//3개밖에 없다고 상정하고 코드 작성//수정시 유의.
     public GameObject ElementNow;//현재 적용중인 속성 스택 //정속성 스택 변화시(추가데미지 변화) 바뀔때의 효과 와 함께 바뀜.
-    public GameObject[] ElementalFX;//스택 변화시 아이콘위치에 적용될 이펙트.
+    public GameObject[] ElementalFX;//스택 변화시 아이콘위치에 적용될 이펙트. 0:화염, 1:물, 2:땅
 
     public Sprite[] BuffsSprites;//버프 스프라이트// 0:공버프 1:방버프 2:방디퍼프 3:속디버프 4:화염스택 5:물스택 6:땅스택
     private SkillType currentSkillT;//현재 적용중인 스킬의 속성.
@@ -20,63 +19,141 @@ public class MonsterStatUI : MonoBehaviour//몬스터의 상단부에 표시될 버프 디버프
     // Update is called once per frame
     async void Update()
     {
-        if (thisMob.isAtkBuffed)
+        //////완전 수작업 방식 공,방,속도,속성 4가지상태에 대해서만 표시하는 경우의 ui
+        if (thisMob.isAtkBuffed)//공격력 아이콘 사용시
         {
-            if(thisMob.isDefBuffed)//공버프 +방버프
+            if (thisMob.isDefBuffed || thisMob.isDefDebuffed)//방어력 관련 아이콘 사용시
             {
-                BuffsUI[0].SetActive(true);//두칸
-                BuffsUI[1].SetActive(true);//두칸
-                BuffsUI[0].GetComponent<SpriteRenderer>().sprite = BuffsSprites[0];//공격버프
-                BuffsUI[1].GetComponent<SpriteRenderer>().sprite = BuffsSprites[1];//방어버프
+                if (thisMob.isDefBuffed)//방어력 버프 사용시
+                {
+                    if (thisMob.isSpeedDebuffed)//속도 디버프 사용시
+                    {
+                        //공버프 + 방어력 버프 + 속도 디버프
+                        BuffsUI[0].SetActive(true);//세칸
+                        BuffsUI[1].SetActive(true);//세칸
+                        BuffsUI[2].SetActive(true);//세칸
+                        BuffsUI[0].GetComponent<SpriteRenderer>().sprite = BuffsSprites[0];//공격력버프
+                        BuffsUI[1].GetComponent<SpriteRenderer>().sprite = BuffsSprites[1];//방어력버프
+                        BuffsUI[2].GetComponent<SpriteRenderer>().sprite = BuffsSprites[3];//속도디버프
+                    }
+                    else//속도 디버프 사용 안할때
+                    {
+                        //공버프 + 방어력버프
+                        BuffsUI[0].SetActive(true);//두칸
+                        BuffsUI[1].SetActive(true);//두칸
+                        BuffsUI[2].SetActive(false);
+                        BuffsUI[0].GetComponent<SpriteRenderer>().sprite = BuffsSprites[0];//공격력버프
+                        BuffsUI[1].GetComponent<SpriteRenderer>().sprite = BuffsSprites[1];//방어력버프
+                    }
+                }
+                else//방어력 디버프 사용시
+                {
+                    if (thisMob.isSpeedDebuffed)//속도 디버프 사용시
+                    {
+                        //공버프 + 방어력 디버프 + 속도 디버프
+                        BuffsUI[0].SetActive(true);//세칸
+                        BuffsUI[1].SetActive(true);//세칸
+                        BuffsUI[2].SetActive(true);//세칸
+                        BuffsUI[0].GetComponent<SpriteRenderer>().sprite = BuffsSprites[0];//공격력버프
+                        BuffsUI[1].GetComponent<SpriteRenderer>().sprite = BuffsSprites[2];//방어력디버프
+                        BuffsUI[2].GetComponent<SpriteRenderer>().sprite = BuffsSprites[3];//속도디버프
+                    }
+                    else//속도 디버프 사용 안할때
+                    {
+                        //공버프+ 방어력 디버프
+                        BuffsUI[0].SetActive(true);//두칸
+                        BuffsUI[1].SetActive(true);//두칸
+                        BuffsUI[2].SetActive(false);
+                        BuffsUI[0].GetComponent<SpriteRenderer>().sprite = BuffsSprites[0];//공격력버프
+                        BuffsUI[1].GetComponent<SpriteRenderer>().sprite = BuffsSprites[2];//방어력디버프
+                    }
+                }
             }
-            else//공버프만
+            else//방어력 관련 아이콘 사용 안할때
             {
-                BuffsUI[0].SetActive(true);//한칸
-                BuffsUI[1].SetActive(false);
-                BuffsUI[0].GetComponent<SpriteRenderer>().sprite = BuffsSprites[0];//공격버프
+                if (thisMob.isSpeedDebuffed)//속도 관련 아이콘 사용시
+                {
+                    //공버프 + 속도 디버프
+                    BuffsUI[0].SetActive(true);//두칸
+                    BuffsUI[1].SetActive(true);//두칸
+                    BuffsUI[2].SetActive(false);
+                    BuffsUI[0].GetComponent<SpriteRenderer>().sprite = BuffsSprites[0];//공격력버프
+                    BuffsUI[1].GetComponent<SpriteRenderer>().sprite = BuffsSprites[3];//속도디버프
+                }
+                else
+                {
+                    //공버프만 있을때
+                    BuffsUI[0].SetActive(true);//한칸
+                    BuffsUI[1].SetActive(false);
+                    BuffsUI[2].SetActive(false);
+                    BuffsUI[0].GetComponent<SpriteRenderer>().sprite = BuffsSprites[0];//공격력버프
+                }
             }
         }
-        else if (thisMob.isDefBuffed)//방버프만
+        else//공버프 없을때
         {
-            BuffsUI[0].SetActive(true);//한칸
-            BuffsUI[1].SetActive(false);
-            BuffsUI[0].GetComponent<SpriteRenderer>().sprite = BuffsSprites[1];//방어버프
-        }//방버프만
-        else
-        {
-            BuffsUI[0].SetActive(false);
-            BuffsUI[1].SetActive(false);
-        }//버프X
-
-
-        if (thisMob.isDefDebuffed)
-        {
-            if (thisMob.isSpeedDebuffed)//방디버프 + 속디버프
+            if (thisMob.isDefBuffed || thisMob.isDefDebuffed)//방어력 관련 아이콘 사용시
             {
-                DebuffsUI[0].SetActive(true);//두칸
-                DebuffsUI[1].SetActive(true);//두칸
-                DebuffsUI[0].GetComponent<SpriteRenderer>().sprite = BuffsSprites[2];//방디버프
-                DebuffsUI[1].GetComponent<SpriteRenderer>().sprite = BuffsSprites[3];//속디버프
+                if(thisMob.isDefBuffed)//방어력 버프 사용시
+                {
+                    if(thisMob.isSpeedDebuffed)//속도 디버프 사용시
+                    {
+                        //방어력 버프 + 속도 디버프
+                        BuffsUI[0].SetActive(true);//두칸
+                        BuffsUI[1].SetActive(true);//두칸
+                        BuffsUI[2].SetActive(false);
+                        BuffsUI[0].GetComponent<SpriteRenderer>().sprite = BuffsSprites[1];//방어력버프
+                        BuffsUI[1].GetComponent<SpriteRenderer>().sprite = BuffsSprites[3];//속도디버프
+                    }
+                    else//속도 디버프 사용 안할때
+                    {
+                        //방어력 버프만
+                        BuffsUI[0].SetActive(true);//한칸
+                        BuffsUI[1].SetActive(false);
+                        BuffsUI[2].SetActive(false);
+                        BuffsUI[0].GetComponent<SpriteRenderer>().sprite = BuffsSprites[1];//방어력버프
+                    }
+                }
+                else//방어력 디버프 사용시
+                {
+                    if(thisMob.isSpeedDebuffed)//속도 디버프 사용시
+                    {
+                        //방어력 디버프 + 속도 디버프
+                        BuffsUI[0].SetActive(true);//두칸
+                        BuffsUI[1].SetActive(true);//두칸
+                        BuffsUI[2].SetActive(false);
+                        BuffsUI[0].GetComponent<SpriteRenderer>().sprite = BuffsSprites[2];//방어력디버프
+                        BuffsUI[1].GetComponent<SpriteRenderer>().sprite = BuffsSprites[3];//속도디버프
+                    }
+                    else//속도 디버프 사용 안할때
+                    {
+                        //방어력 디버프만
+                        BuffsUI[0].SetActive(true);//한칸
+                        BuffsUI[1].SetActive(false);
+                        BuffsUI[2].SetActive(false);
+                        BuffsUI[0].GetComponent<SpriteRenderer>().sprite = BuffsSprites[2];//방어력디버프
+                    }
+                }
             }
-            else//방디버프만
+            else//방어력 관련 아이콘도 사용 안할때
             {
-                DebuffsUI[0].SetActive(true);//한칸
-                DebuffsUI[1].SetActive(false);
-                DebuffsUI[0].GetComponent<SpriteRenderer>().sprite = BuffsSprites[2];//방디버프
+                if(thisMob.isSpeedDebuffed)//속도 관련 아이콘 사용시
+                {
+                    //속도 디버프만
+                    BuffsUI[0].SetActive(true);//한칸
+                    BuffsUI[1].SetActive(false);
+                    BuffsUI[2].SetActive(false);
+                    BuffsUI[0].GetComponent<SpriteRenderer>().sprite = BuffsSprites[3];//속도디버프
+                }
+                else//아무런 버프 디버프도 없을때
+                {
+                    //아무런 아이콘 없음
+                    BuffsUI[0].SetActive(false);
+                    BuffsUI[1].SetActive(false);
+                    BuffsUI[2].SetActive(false);
+                }
             }
-        }
-        else if (thisMob.isSpeedDebuffed)//속디버프만
-        {
-            DebuffsUI[0].SetActive(true);//한칸
-            DebuffsUI[1].SetActive(false);
-            DebuffsUI[0].GetComponent<SpriteRenderer>().sprite = BuffsSprites[3];//속디버프
-        }//속디버프만
-        else
-        {
-            DebuffsUI[0].SetActive(false);
-            DebuffsUI[1].SetActive(false);
-        }//디버프X
-
+        }//공버프 없을때의 경우
 
         if (thisMob.stackedElement != currentSkillT)
         {
@@ -107,20 +184,29 @@ public class MonsterStatUI : MonoBehaviour//몬스터의 상단부에 표시될 버프 디버프
     private async UniTask TurnElementFire()
     {
         //불꽃 아이콘 효과
+        ElementalFX[0].SetActive(true);
         await UniTask.Delay(800);
         ElementNow.GetComponent<SpriteRenderer>().sprite = BuffsSprites[4];
+        await UniTask.Delay(300);
+        ElementalFX[0].SetActive(false);
     }
     private async UniTask TurnElementWater()
     {
         //물 아이콘 효과
+        ElementalFX[1].SetActive(true);
         await UniTask.Delay(800);
         ElementNow.GetComponent<SpriteRenderer>().sprite = BuffsSprites[5];
+        await UniTask.Delay(300);
+        ElementalFX[1].SetActive(false);
     }
     private async UniTask TurnElementWood()
     {
         //땅 아이콘 효과
         await UniTask.Delay(800);
+        ElementalFX[2].SetActive(true);
         ElementNow.GetComponent<SpriteRenderer>().sprite = BuffsSprites[6];
+        await UniTask.Delay(300);
+        ElementalFX[2].SetActive(false);
     }
 
 }
