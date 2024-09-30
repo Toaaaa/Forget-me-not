c3f3de961eb25859fe2026b2046f9b6ad20a4343
minjun Kim <k980128@pusan.ac.kr>
2024-09-30 21:07:36 +0900

    feat : added the start delay on the skill lighting system.

--- Playable/skill extra/SFXLigthing.cs
+++ Playable/skill extra/SFXLigthing.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 using DG.Tweening;
+using Cysharp.Threading.Tasks;
 
 public class SFXLigthing : MonoBehaviour
 {
     public Light2D light2D;
+    [SerializeField]
+    private float L_Time0;//기본은 0, 라이트를 켜기전 대기시간이 필요할 경우 사용.    
     [SerializeField]
     private float L_Time1;//번쩍 하는 시간 alpha가 0에서 255까지 가는 시간
     [SerializeField]
@@ -22,13 +25,14 @@ public class SFXLigthing : MonoBehaviour
     {
         if (light2D == null)
             light2D = GetComponent<Light2D>();
-        Light();
+        Light().Forget();
     }
 
-    private void Light()
+    private async UniTask Light()
     {
         Color lightColor = light2D.color;
-
+        
+        await UniTask.Delay((int)(L_Time0 * 1000));//대기시간
         // 알파 값을 0에서 255로 증가시키기
         DOTween.To(() => lightColor.a, x => lightColor.a = x, 1f, L_Time1)
             .OnUpdate(() => light2D.color = lightColor)
